2a8f528681eea733f26e3e43b02fbb4769f29c70
sblack556 <81171335+sblack556@users.noreply.github.com>
2021-06-19 14:57:42 -0400

    Add files via upload

---
+++ Web Project Template/scripts/myscripts.js
@@ -0,0 +1,23 @@
+// JavaScript File Template
+"use strict";
+
+
+//JavaScript is a programming language used primarily by web browsers to create function.  JavaScript creates a dynamic and interactive experience for the user.  
+
+
+// Every JavaScript file should have "use strict" at the very top.  "use strict" indicates the code should be in "strict mode".  This helps you to write cleaner code.  For example, preventing you from using undeclared variables, etc.
+
+
+// Below is an example of JavaScript that an HTML file can access to turn a lightbulb on and off.
+
+// document.getElementById("offbutton").onclick = turnoffLight;
+
+// function turnoffLight() {
+//     document.getElementById("lightbulb").src="images/light-off.png";
+// };
+
+// document.getElementById("onbutton").onclick = turnonLight;
+
+// function turnonLight() {
+//     document.getElementById("lightbulb").src="images/light-on.png";
+// };
